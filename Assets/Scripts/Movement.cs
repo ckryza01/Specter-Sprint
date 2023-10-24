8c8c729a895b197f89bca769ea553f3ecf17132f
Caleb Kryza <calebkryza@gmail.com>
2023-10-24 15:45:21 -0700

    Added movement/running/jumping and camera control

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+
+{
+
+    [SerializeField] Transform playerCamera;
+    [SerializeField][Range(0.0f, 0.5f)] float mouseSmoothTime = 0.03f;
+    [SerializeField] bool cursorLock = true;
+    [SerializeField] float mouseSensitivity = 3.5f;
+    [SerializeField] float speed = 6.0f;
+    [SerializeField][Range(0.0f, 0.5f)] float moveSmoothTime = 0.03f;
+    [SerializeField] float gravity = -9.8f;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] LayerMask ground;
+    [SerializeField] float maxStamina = 100.0f;
+    [SerializeField] float staminaRegenRate = 1.0f;
+    [SerializeField] float staminaLossRate = 10.0f;
+
+
+
+    public float jumpHeight = 6f;
+    float sprintSpeed = 12.0f;
+    [SerializeField]
+    float currentStamina;
+    float velocityY;
+    bool isGrounded, isSprinting;
+
+    float cameraCap;
+    Vector2 currentMouseDelta, currentMouseDeltaVelocity, currentDir, currentDirVelocity;
+    CharacterController controller;
+    Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentStamina = maxStamina;
+        controller = GetComponent<CharacterController>();
+        if (cursorLock)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMouse();
+        UpdateMove();
+    }
+
+    void UpdateMove()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, 0.2f, ground);
+
+        Vector2 targetDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        targetDir.Normalize();
+
+        if (Input.GetButton("Sprint"))
+        {
+            isSprinting = true;
+        }
+        else
+        {
+            isSprinting = false;
+        }
+
+        float currentSpeed = isSprinting && currentStamina > 0f ? sprintSpeed : speed;
+
+        
+
+        currentDir = Vector2.SmoothDamp(currentDir, targetDir, ref currentDirVelocity, moveSmoothTime);
+
+        velocityY += gravity * 2f * Time.deltaTime;
+
+        Vector3 velocity = (transform.forward * currentDir.y + transform.right * currentDir.x) * currentSpeed + Vector3.up * velocityY;
+
+        controller.Move(velocity * Time.deltaTime);
+
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            velocityY = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        if (isGrounded! && controller.velocity.y < -1f)
+        {
+            velocityY = -8f;
+        }
+
+        if (isSprinting)
+        {
+            currentStamina -= staminaLossRate * Time.deltaTime;
+            currentStamina = Mathf.Max(0, currentStamina);
+        }else if(currentStamina < maxStamina)
+        {
+            currentStamina += staminaRegenRate * Time.deltaTime;
+            currentStamina = Mathf.Min(maxStamina, currentStamina);
+        }
+    }
+
+    void UpdateMouse()
+    {
+        Vector2 targetMouseDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        currentMouseDelta = Vector2.SmoothDamp(currentMouseDelta, targetMouseDelta, ref currentMouseDeltaVelocity, mouseSmoothTime);
+
+        cameraCap -= currentMouseDelta.y * mouseSensitivity;
+
+        cameraCap = Mathf.Clamp(cameraCap, -90.0f, 90.0f);
+
+        playerCamera.localEulerAngles = Vector3.right * cameraCap;
+
+        transform.Rotate(Vector3.up * currentMouseDelta.x * mouseSensitivity);
+    }
+}
