82a7ab66fd0239601c9a9dbf15b2b52644999c97
Rachel <rbreinho@asu.edu>
2023-11-15 13:46:40 -0700

    Moster attack and blue orbs

--- Assets/Scripts/MonsterSpawn.cs
+++ Assets/Scripts/MonsterSpawn.cs
@@ -16,19 +16,8 @@ public class MonsterSpawn : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             Vector3 spawnPosition = transform.position;
-            GameObject spawnedMonster = Instantiate(objectToSpawn, spawnPosition, Quaternion.identity);
+            Instantiate(objectToSpawn, spawnPosition, Quaternion.identity);
 
-            //change animation to walk
-            Animation animationComponent = spawnedMonster.GetComponent<Animation>();
-            if (animationComponent != null)
-            {
-                animationComponent.Play("Walk");
-            }
-            
-            //Chase player
-            WaypointFollower waypointFollower = spawnedMonster.AddComponent<WaypointFollower>();
-            GameObject player = GameObject.FindGameObjectWithTag("Player");
-            waypointFollower.addWaypoint(player);
         }
     }  
             
